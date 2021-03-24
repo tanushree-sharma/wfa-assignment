24b329c2afc17e577754a57f7420668b37124def
tanushree-sharma <tanushree@hotmail.ca>
2021-03-23 23:52:27 -0400

    trying to get the formulation working

--- formulation.m
+++ formulation.m
@@ -5,10 +5,19 @@ clear A;
 A = all_pairings;
 
 [n,m]=size(A);
-f=cost_matrix;
+c=cost_matrix;
+f=ones(m,1);
 b=ones(n,1);
 
-x=intlinprog(f,[1:m],A,b, [], [],0*f,0*f+1);
+ineq = ones(1,m);
+ineq_rhs = ones(1,1)
+
+% objective function coefficients,intcon, linear inequality constraint
+% linear inequality RHS, linear equality constraint, linear inequality RHS
+% lb, ub
+x=intlinprog(c,[1:m],[],[],A,b,0*f,0*f+1);
+% no feasible solution: x=intlinprog(c,[1:m],ineq,ineq_rhs,A,b,0*f,0*f+1);
+
 
 i=find(x==1);
 optimal_solution= A(:,i)
