0af529ca1d6eaeaf32ca6fcde77c60d710f395b4
toni-li <toni.li@uwaterloo.ca>
2021-03-24 19:48:03 -0400

    one optimal solution

--- scformulationv2.m
+++ scformulationv2.m
@@ -2,13 +2,15 @@ assignCostsv2;
 gen_pairingsv2;
 
 A = Pfinal;
-
 [n,m]=size(A);
 f=cost;
 b=ones(n,1);
 
+Aeq = ones(1,m);
+beq = 1;
+
 %x=bintprog(f,[],[],A,b);
-x=intlinprog(f,[1:m],[],[],A,b,0*f,0*f+1);
+x=intlinprog(f,[1:m],A,b,Aeq,beq,0*f,0*f+1);
 
 
 i=find(x==1);
