7cbad76e4eb410dec12c92d348c972e5001b9f00
toni-li <toni.li@uwaterloo.ca>
2021-04-06 11:47:50 -0400

    sensitivity analysis for sc model

--- scformulationv2.m
+++ scformulationv2.m
@@ -7,11 +7,11 @@ f=cost;
 b=ones(n,1);
 
 Aeq = ones(1,m);
-beq = 1;
+beq = 1; % changed to 4 for the sensitivity analysis
 
 %x=bintprog(f,[],[],A,b);
 x=intlinprog(f,[1:m],A,b,Aeq,beq,0*f,0*f+1);
 
 
-i=find(x==1);
+i=find(x>0);
 optimal_solution= A(:,i);
